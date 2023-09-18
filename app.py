8ab3ef3c91a16196b9952f33fab42fb83e8b1066
Moudathirou Ben Saindou <kymsaindou@gmail.com>
2023-09-18 05:08:40 +0100

    our web app is ready

--- app.py
+++ app.py
@@ -33,6 +33,16 @@ def predict_api():
 
     return jsonify(float(output[0]))
 
+@app.route('/predict',methods=["POST"])
+def predict():
+    data=[float(x) for  x  in request.form.values()]
+
+    output =model.predict(np.array(data).reshape(1,-1))[0]
+
+    return render_template("home.html",prediction_text="the hg prediction is {}".format(output))
+
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
